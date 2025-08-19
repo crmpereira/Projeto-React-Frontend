c6dc5832787ef6288d2c40f501119249f5e902a3
crmpereira <cesar.pereiram@gmail.com>
2025-08-19 13:54:21 -0300

    Ajustes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Login from "./login";  // importa o componente Login
 import "./App.css";           // mantém o estilo padrão se quiser
 import Estados from './components/Estados';
 
+
 function App() {
   return (
     <div className="App">
